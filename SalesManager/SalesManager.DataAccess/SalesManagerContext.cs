5901c2be3c979ef4bf17c8082aab89e09a7bb68a
Gonzalo Lorieto <lorietogonzalo@gmail.com>
2019-02-13 22:08:53 -0300

    Fluent api on modelBuilder

--- SalesManager/SalesManager.DataAccess/SalesManagerContext.cs
+++ SalesManager/SalesManager.DataAccess/SalesManagerContext.cs
@@ -11,7 +11,15 @@ namespace SalesManager.DataAccess
     {
         public SalesManagerContext(DbContextOptions<SalesManagerContext> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder builder) => base.OnModelCreating(builder);
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>()
+                .HasIndex(p => new { p.Name })
+                .IsUnique(true);
+
+        }
 
         public DbSet<Client> Clients { get; set; }
         public DbSet<Item> Items { get; set; }

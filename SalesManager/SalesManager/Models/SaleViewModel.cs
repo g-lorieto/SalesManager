6638bf0f028cf039f8aa9cdfeaf368cbe4a542c8
Gonzalo Lorieto <lorietogonzalo@gmail.com>
2019-02-16 04:13:53 -0300

    trying to create a Sale and some Items

--- SalesManager/SalesManager/Models/SaleViewModel.cs
+++ SalesManager/SalesManager/Models/SaleViewModel.cs
@@ -11,12 +11,15 @@ namespace SalesManager.Models
         public List<Client> Clients { get; set; }
         public List<Product> Products { get; set; }
         public Sale Sale { get; set; }
+        public Item Item { get; set; }
+        public int ClientId { get; set; }
+
 
         public SaleViewModel(List<Client> clients, List<Product> products)
         {
             Clients = clients;
             Products = products;
+            Item = new Item();
         }
-
     }
 }

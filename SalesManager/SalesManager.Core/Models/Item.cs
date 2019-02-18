f496b1388d350eb5b4676d117efb6ac219f2c7f5
Gonzalo Lorieto <lorietogonzalo@gmail.com>
2019-02-18 10:45:44 -0300

    Removing shadow keys

--- SalesManager/SalesManager.Core/Models/Item.cs
+++ SalesManager/SalesManager.Core/Models/Item.cs
@@ -2,6 +2,7 @@
 {
     public class Item : BaseEntity
     {
+        public int ProductId { get; set; }
         public Product Product { get; set; }
         public decimal Quantity { get; set; }
         public decimal Price { get; set; }
--- SalesManager/SalesManager.Core/Models/Sale.cs
+++ SalesManager/SalesManager.Core/Models/Sale.cs
@@ -7,6 +7,7 @@ namespace SalesManager.Core.Models
     {
         public DateTime Date{ get; set; }
         public DateTime DeliveryDay { get; set; }
+        public int ClientId { get; set; }
         public Client Client { get; set; }
         public bool IsPayed { get; set; }
         public bool IsDelivered { get; set; }
